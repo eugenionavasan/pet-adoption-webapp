65debcddbddfe98d1b2bf8c333fd4278852fdce5
Eugenio Navajo <eugenio.navasan@gmail.com>
2024-10-06 12:55:43 +0200

    homepage styled

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
-import Pet from "./pet"
 import useBreedList from './useBreedList';
+import Results from './Results';
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"]
 
 const SearchParams = () => {
@@ -47,11 +47,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {
-        pets.map(pet=> (
-          <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
-        ))
-      }
+      <Results pets={pets} />
     </div>
   );
 }
