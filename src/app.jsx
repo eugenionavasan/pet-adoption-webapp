1365bd3b53d74b1bb7a0a7784bb3224c7d15be72
Eugenio Navajo <eugenio.navasan@gmail.com>
2024-10-09 10:08:27 +0200

    Added routing, details page

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,21 @@
 import { createRoot } from 'react-dom/client';
+import Details from './Details';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import SearchParams from './SearchParams';
 
 const App = () => {
   return (
-  <div>
-    <h1>Adopt me!</h1>
-    <SearchParams />
-  </div>
+    <div>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
 )};
 
 const container = document.getElementById("root");
