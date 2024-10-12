9900fc848c76c232d2c5bace206801af8a9bca37
Eugenio Navajo <eugenio.navasan@gmail.com>
2024-10-12 02:26:23 +0200

    fix working for manual deployment, still not automatic deployment

--- src/pet.jsx
+++ src/pet.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
-const Pet = ({name, animal, breed, images, location, id}) => {
-  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "https://pets-images.dev-apis.com/pets/none.jpg";
   if (images.length) {
     hero = images[0];
   }
@@ -12,10 +12,12 @@ const Pet = ({name, animal, breed, images, location, id}) => {
       </div>
       <div className="info">
         <h1>{name}</h1>
-        <h2>{animal} - {breed} - {location}</h2>
+        <h2>
+          {animal} - {breed} - {location}
+        </h2>
       </div>
     </Link>
-  )
-}
+  );
+};
 
 export default Pet;
