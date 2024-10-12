ac5c0e86487702d29f9711ace3bf69e17b695c68
Eugenio Navajo <eugenio.navasan@gmail.com>
2024-10-12 10:06:41 +0200

    trying automatic import

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,19 @@
-import { createRoot } from 'react-dom/client';
 import { useState } from 'react';
 import Details from './Details';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import SearchParams from './SearchParams';
 import AdoptedPetContext from './AdoptedPetContext';
-import './style.css'
+import './style.css';
 
 const queryClient = new QueryClient({
   defaultOptions: {
-    queries:{
+    queries: {
       staleTime: Infinity,
       cacheTime: Infinity,
-    }
-  }
-})
+    },
+  },
+});
 
 const App = () => {
   const adoptedPet = useState(null);
@@ -35,8 +34,7 @@ const App = () => {
         </QueryClientProvider>
       </BrowserRouter>
     </div>
-)};
+  );
+};
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+export default App;
